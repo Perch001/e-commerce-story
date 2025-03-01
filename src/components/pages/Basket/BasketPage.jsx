e35f921f759d86f47e565a069ffeedbbaac85935
Daniyar <pssghoil@gmail.com>
2025-03-01 14:16:56 +0500

    null card

--- src/components/pages/Basket/BasketPage.jsx
+++ src/components/pages/Basket/BasketPage.jsx
@@ -7,7 +7,6 @@ import {fetchCardProducts} from "../../../store/CardSlice.js";
 const BasketPage = () => {
     const dispatch = useDispatch();
     const cards = useSelector(state => state.card.cards);
-    console.log(cards);
     useEffect(() => {
         dispatch(fetchCardProducts())
     },[dispatch])
@@ -15,9 +14,14 @@ const BasketPage = () => {
     return (
         <div className="container mx-auto flex justify-between mt-20">
             <div className="flex-1 mr-40">
-                {cards.map((product) => (
-                    <BasketList {...product} key={product.id} />
-                ))}
+                {cards.length > 0
+                    ?
+                        cards.map((product) => (
+                            <BasketList {...product} key={product.id} />
+                        ))
+                    :
+                        <div>Card is null</div>
+                }
             </div>
             <BasketCheck />
         </div>
